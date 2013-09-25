7aa28e51fa2daa732f4236bc111b6997b1871948
Lukas Eipert <git@valedictorum.de>
2013-09-25 16:27:01 +0200

    Initial commit.

---
+++ scripts.php
@@ -0,0 +1,22 @@
+<?php
+function execInBackground($cmd) { 
+    if (substr(php_uname(), 0, 7) == 'Windows'){ 
+        pclose(popen('start /B '. $cmd, "r"));  
+    } 
+    else { 
+        exec($cmd . ' > /dev/null &');   
+    } 
+}
+if(isset($_GET['call'])){
+    switch ($_GET['call']) {
+        case 'reboot':
+            execInBackground('sudo shutdown -r now');
+            break;
+        case 'shutdown':
+            execInBackground('sudo shutdown -h now');
+            break;
+        case 'pull':
+            execInBackground('git pull');
+            break;
+    }
+}
