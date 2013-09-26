e6838bd04397a5701de75b9a5f8ad57c1715d60b
Lukas Eipert <git@valedictorum.de>
2013-09-26 15:55:54 +0200

    Escaped brackets in regex, Changed needle and haystack in strpos.

--- scripts.php
+++ scripts.php
@@ -26,11 +26,11 @@ if(isset($_GET['call'])){
         case 'status':
             $return = array('return'=>'','crashplan'=>false,'hdd'=>false,'debug'=>'');
             exec('/usr/local/crashplan/bin/CrashPlanEngine status > /var/www/output.log 2>&1');
-            if (preg_match('/CrashPlan\s+Engine\s+(pid \d+)\s+is\s+running/', file_get_contents('output.log'))){
+            if (preg_match('/CrashPlan Engine \(pid \d+\) is running/', file_get_contents('output.log'))){
             $return['crashplan'] = true;
             }
             exec('[ ! -e /media/backup/.backup ]; echo "HDD_there_$?" >> /var/www/output.log 2>&1');
-            if (strpos('HDD_there_1', file_get_contents('output.log'))!==false){
+            if (strpos(file_get_contents('output.log'),'HDD_there_1')!==false){
             $return['hdd'] = true;
             }
             exec('cat /proc/swaps | grep /dev >> /var/www/output.log 2>&1');
